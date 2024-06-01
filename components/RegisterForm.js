afa90da131467b0f74758d48682128e61f671954
Nathan <nathopp@gmail.com>
2024-06-01 17:03:18 -0500

    setup

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -17,10 +17,18 @@ function RegisterForm({ user, updateUser }) {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="formBasicBio">
         <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Control
+          as="textarea"
+          name="bio"
+          required
+          placeholder="Enter your Bio"
+          onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))}
+        />
+        <Form.Text className="text-muted">
+          Let other gamers know a little bit about you...
+        </Form.Text>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
--- utils/auth.js
+++ utils/auth.js
@@ -3,11 +3,8 @@ import 'firebase/auth';
 import { clientCredentials } from './client';
 
 const checkUser = (uid) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/checkuser`, {
-    method: 'POST',
-    body: JSON.stringify({
-      uid,
-    }),
+  fetch(`${clientCredentials.databaseURL}/checkuser/${uid}`, {
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
@@ -18,7 +15,7 @@ const checkUser = (uid) => new Promise((resolve, reject) => {
 });
 
 const registerUser = (userInfo) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/register`, {
+  fetch(`${clientCredentials.databaseURL}/user`, {
     method: 'POST',
     body: JSON.stringify(userInfo),
     headers: {
